d1b6a7347739056e9a5fadf1b121be25bf14360b
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2025-12-11 21:06:21 +0530

    Time: 1 ms (99.83%), Space: 55.6 MB (26.65%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,18 +1,23 @@
+//Moores voting algo-->technodage
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
-        Map<Integer,Integer> map=new HashMap<>();
 
-        for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
+        int candidate = 0;
+        int point = 0;
+        for (int num : nums) {
+            if (point == 0) {
+                candidate = num;
+            }
 
-        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-            if(entry.getValue()> n/2){
-                return entry.getKey();
+            if (candidate == num) {
+                point++;
+            } else {
+                point--;
             }
         }
 
-        return -1;
+        return candidate;
     }
+
 }
+
