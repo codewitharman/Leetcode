d1292809a4d67da3cc122fbf3fbf9b8ca976db53
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-01-04 14:51:52 +0530

    Time: 672 ms (56.37%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
 select 
 round(100*sum(case when b.min_order_date = b.min_delivery_date then 1 else 0 end)/count(*), 2)
 as immediate_percentage
