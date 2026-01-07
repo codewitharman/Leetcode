dfcbe76b9cfa7625c7fcab8d08468fe6c02627d4
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-01-07 15:37:05 +0530

    Time: 22 ms (5.73%), Space: 93.8 MB (5.46%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,16 +1,13 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        Set<Integer> set1 = new HashSet<>();
-        Set<Integer> set2 = new HashSet<>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int num : nums1) {
-            set1.add(num);
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
         for (int num : nums2) {
-            if (set1.contains(num)) {
+            if (map.containsKey(num)) {
                 return num;
-            } else {
-                set2.add(num);
             }
         }
         return -1;
