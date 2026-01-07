695092d013e64f96c9b00f764c6f49624daeefef
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-01-07 17:28:32 +0530

    Time: 1 ms (42.49%), Space: 42.8 MB (50.83%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -1,17 +1,13 @@
 class Solution {
     public String sortSentence(String s) {
         String[] words = s.split(" ");
-        Map<Integer,String> map=new HashMap<>();
-        for(String word :words){
-            int pos=word.charAt(word.length() - 1) - '0';  //position
-            map.put(pos,word.substring(0, word.length() - 1)); //wordwithouthenumber
-        }
+        List<String> list = new ArrayList<>(Collections.nCopies(words.length, ""));
 
-        StringBuilder sb = new StringBuilder();
-        for(int i = 1; i <= words.length; i++){ 
-            sb.append(map.get(i)).append(" ");   //get the index 1 word to n and append them with spaces in it 
+        for (String word : words) {
+            int pos = word.charAt(word.length() - 1) - '1';
+            list.set(pos, word.substring(0, word.length() - 1));
         }
-        return sb.toString().trim();  //remove the ending/trailing white space
+        return String.join(" ", list);
 
     }
 }
