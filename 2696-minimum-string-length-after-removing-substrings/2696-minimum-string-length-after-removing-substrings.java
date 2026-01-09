7a72ca99089e7bbca1c4645d9154ebd42c56e181
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-01-09 17:49:29 +0530

    Time: 3 ms (95.74%), Space: 44.6 MB (85.63%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
@@ -1,24 +1,15 @@
 class Solution {
     public int minLength(String s) {
         StringBuilder sb = new StringBuilder();
+
         for (char ch : s.toCharArray()) {
-            if (sb.length() > 0 && ch == 'B') {
-                if (sb.charAt(sb.length() - 1) == 'A') {
-                    sb.deleteCharAt(sb.length() - 1);
-                } else {
-                    sb.append(ch);
-                }
-            } else if (sb.length() > 0 && ch == 'D') {
-                if (sb.charAt(sb.length() - 1) == 'C') {
-                    sb.deleteCharAt(sb.length() - 1);
-                } else {
-                    sb.append(ch);
-                }
+            int n = sb.length();
+            if (n > 0 && ((ch == 'B' && sb.charAt(n - 1) == 'A') || (ch == 'D' && sb.charAt(n - 1) == 'C'))) {
+                sb.deleteCharAt(n - 1);
             } else {
                 sb.append(ch);
             }
         }
-
         return sb.length();
     }
 }
