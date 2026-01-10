28ed03b3c459e7ceeb5b346fdd239e26d68eb2ab
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-01-11 00:27:53 +0530

    Time: 4 ms (79%), Space: 44.4 MB (70.84%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,17 +3,19 @@ class Solution {
         if (s.length() != t.length()) {
             return false;
         }
-
         int nums[] = new int[26];
-        for (char ch : s.toCharArray()) {
-            nums[ch - 'a']++;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            int index = ch - 'a';
+            nums[index] = nums[index] + 1;
         }
-        for (char ch : t.toCharArray()) {
-            nums[ch - 'a']--;
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            int index = ch - 'a';
+            nums[index] = nums[index] - 1;
         }
-
-        for (int ch : nums) {
-            if (ch != 0) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
                 return false;
             }
         }
