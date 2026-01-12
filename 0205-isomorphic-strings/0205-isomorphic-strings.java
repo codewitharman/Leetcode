ef44501ff5f017ca59df8b9b9c86b936b617e17d
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-01-12 16:00:49 +0530

    Time: 16 ms (40.68%), Space: 44.1 MB (29.29%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,31 +1,28 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
-
         if (s.length() != t.length())
             return false;
 
-        Map<Character, Character> sToT = new HashMap<>();
-        Map<Character, Character> tToS = new HashMap<>();
+        Map<Character, Character> map1 = new HashMap<>();
+        Map<Character, Character> map2 = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
-            char c1 = s.charAt(i);
-            char c2 = t.charAt(i);
-
-            if (sToT.containsKey(c1)) {
-                if (sToT.get(c1) != c2)
+            char charS = s.charAt(i);
+            char charT = t.charAt(i);
+            if (map1.containsKey(charS)) {
+                if (map1.get(charS) != charT) {
                     return false;
+                }
             } else {
-                sToT.put(c1, c2);
+                map1.put(charS, charT);
             }
 
-            if (tToS.containsKey(c2)) {
-                if (tToS.get(c2) != c1)
+            if (map2.containsKey(charT)) {
+                if (map2.get(charT) != charS) {
                     return false;
+                }
             } else {
-                tToS.put(c2, c1);
+                map2.put(charT, charS);
             }
         }
 
