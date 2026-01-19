d1ae8b7b6ff5e5ce9637f4ebf609053ebabeb3fc
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-01-19 14:35:19 +0530

    Time: 1 ms (80.93%), Space: 42.7 MB (79.26%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,33 +1,24 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        String[] words = s.split("[\\s+]");
-        //System.out.println(Arrays.toString(wordsArr));
-        //System.out.println(wordsArr.length);
-        //System.out.println(pattern.length());
-        if (words.length != pattern.length()) {
-            return false;
-        }
+        HashMap<Character, String> charToWord = new HashMap<>();
+        HashMap<String, Character> wordToChar = new HashMap<>();
 
-        Map<Character, String> charToWord = new HashMap<>();
-        Map<String, Character> wordToChar = new HashMap<>();
+        String[] words = s.split(" ");
+
+        if (words.length != pattern.length())
+            return false;
 
         for (int i = 0; i < pattern.length(); i++) {
             char ch = pattern.charAt(i);
             String word = words[i];
 
             if (charToWord.containsKey(ch)) {
-                if (!charToWord.get(ch).equals(word)) {
+                if (!charToWord.get(ch).equals(word))
                     return false;
-                }
             } else {
+                if (wordToChar.containsKey(word))
+                    return false;
                 charToWord.put(ch, word);
-            }
-
-            if (wordToChar.containsKey(word)) {
-                if (wordToChar.get(word) != ch) {
-                    return false;
-                }
-            } else {
                 wordToChar.put(word, ch);
             }
         }
