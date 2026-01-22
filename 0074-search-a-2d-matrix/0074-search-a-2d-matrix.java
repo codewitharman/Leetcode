31c4e7a5df6743ac58803b42a50a6d9e05808296
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-01-22 10:26:04 +0530

    Time: 0 ms (100%), Space: 44.1 MB (15.43%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,10 +1,37 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+
+        if (matrix.length == 0 || matrix == null || matrix[0].length == 0) {
             return false;
         }
 
-        return Stream.of(matrix).flatMapToInt(row -> java.util.Arrays.stream(row)).anyMatch(num -> num == target);
+        int n = 0;
+        for (int[] row : matrix) {
+            n = n + row.length;
+        }
 
+        int[] arr = new int[n];
+        int index = 0;
+        for (int[] row : matrix) {
+            for (int element : row) {
+                arr[index++] = element;
+            }
+        }
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return true;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return false;
     }
 }
