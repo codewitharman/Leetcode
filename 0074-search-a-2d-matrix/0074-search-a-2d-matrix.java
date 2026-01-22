908171d0b2a9a1784680efb68735dfa3c367f6d2
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-01-22 11:08:32 +0530

    Time: 0 ms (100%), Space: 42.8 MB (99.93%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,35 +1,31 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-
-        if (matrix.length == 0 || matrix == null || matrix[0].length == 0) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
             return false;
-        }
 
-        int n = 0;
-        for (int[] row : matrix) {
-            n = n + row.length;
-        }
-
-        int[] arr = new int[n];
-        int index = 0;
-        for (int[] row : matrix) {
-            for (int element : row) {
-                arr[index++] = element;
-            }
-        }
-
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        //System.out.println(rows);
+        //System.out.println(cols);
         int start = 0;
-        int end = arr.length - 1;
+        int end = rows * cols - 1;
 
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            if (arr[mid] == target) {
+            int row = mid / cols; //index of row
+            int col = mid % cols; //index of col  
+            //System.out.println(row);
+            //System.out.println(col);
+
+            int val = matrix[row][col];
+            if (val == target) {
                 return true;
-            } else if (target > arr[mid]) {
+            } else if (target > val) {
                 start = mid + 1;
             } else {
                 end = mid - 1;
             }
+
         }
 
         return false;
