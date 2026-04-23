7607f0747a09f3f346d3c3c7875296e4fd928ac1
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-04-23 20:09:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.java
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.java
@@ -1,26 +1,15 @@
 class Solution {
     public int secondHighest(String s) {
-        HashSet<Integer> set = new HashSet<>();
-        for (char ch : s.toCharArray()) {
-            if (Character.isDigit(ch)) {
-                set.add(ch - '0');
-            }
-        }
-
-        int nums[] = new int[set.size()];
-
-        int index = 0;
-        for (int num : set) {
-            nums[index++] = num;
-        }
-        int largest = -1, secondLargest = -1;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > largest) {
-                secondLargest = largest;
-                largest = nums[i];
-            } else if (nums[i] > secondLargest) {
-                secondLargest = nums[i];
+        char[] arr = s.toCharArray();
+        int secondLargest = -1, largest = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (Character.isDigit(arr[i])) {
+                if (arr[i] - '0' > largest) {
+                    secondLargest = largest;
+                    largest = arr[i] - '0';
+                } else if (arr[i] - '0' > secondLargest && arr[i] - '0' < largest) {
+                    secondLargest = arr[i] - '0';
+                }
             }
         }
 
