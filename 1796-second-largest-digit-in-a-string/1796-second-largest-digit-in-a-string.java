4886cc07584a92feebd90995d79a38672b0ba991
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-04-23 20:21:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.java
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.java
@@ -1,18 +1,21 @@
 class Solution {
     public int secondHighest(String s) {
-        char[] arr = s.toCharArray();
-        int secondLargest = -1, largest = -1;
-        for (int i = 0; i < arr.length; i++) {
-            if (Character.isDigit(arr[i])) {
-                if (arr[i] - '0' > largest) {
-                    secondLargest = largest;
-                    largest = arr[i] - '0';
-                } else if (arr[i] - '0' > secondLargest && arr[i] - '0' < largest) {
-                    secondLargest = arr[i] - '0';
+        char [] digitsArrays=s.replaceAll("[^0-9]","").toCharArray(); 
+        System.out.println(Arrays.toString(digitsArrays));
+        int largest=-1, secondLargest=-1;
+
+        
+        for(char ch: digitsArrays){
+            if(Character.isDigit(ch)){
+                int num=ch-'0';
+                if(num>largest){
+                    secondLargest=largest;
+                    largest=num;
+                }else if(num<largest && num>secondLargest){
+                    secondLargest=num;    
                 }
             }
         }
-
         return secondLargest;
     }
 }
