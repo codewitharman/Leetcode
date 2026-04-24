7ec3ac4dbc7a8061e642f2fd1a29ae02687578f8
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-04-24 23:20:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,12 +1,13 @@
 class Solution {
     public void reverseString(char[] s) {
-        String res=new String(s);
-        StringBuilder sb=new StringBuilder(res);
-        sb.reverse();
-        String temp=sb.toString();
-        for(int i=0;i<s.length;i++)
-        {
-            s[i]=temp.charAt(i);
+        //Collections.reverse(Arrays.asList(s));
+        int start = 0, end = s.length - 1;
+        while (start < end) {
+            char ch = s[start];
+            s[start] = s[end];
+            s[end] = ch;
+            start++;
+            end--;
         }
     }
 }
