62bc5efe54e3c991782ed71ad5cdf2ab68947186
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-04-28 23:52:13 +0530

    Time: 1 ms (65.29%), Space: 43.1 MB (73.64%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.java
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -3,13 +3,8 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         int n = nums.length;
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i].charAt(i) == '0') {
-                sb.append('1');
-            } else {
-                sb.append('0');
-            }
-        }
+        for (int i = 0; i < n; i++)
+            sb.append(nums[i].charAt(i) == '0' ? 1 : 0);
 
         return sb.toString();
     }
