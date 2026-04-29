8fd4a0079d0fae597a4f84005506816020130715
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-04-29 11:06:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
-        Arrays.sort(nums);
-
+        //Arrays.sort(nums);
+        bubbleSort(nums);
         List<Integer> list = new ArrayList<>();
 
         for (int i = 0; i < nums.length; i++) {
@@ -11,5 +11,17 @@ class Solution {
         }
 
         return list;
+    }
+
+    public void bubbleSort(int nums[]) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
     }
 }
