f40862350da46e5d852c8f250cbcdc4fc6188556
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-05-18 16:13:17 +0530

    Time: 0 ms (100%), Space: 45 MB (67.68%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,25 +1,20 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int num : nums1) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        int[] freq = new int[1001];
+        for (int i = 0; i < nums1.length; i++) {
+            freq[nums1[i]]++;
         }
 
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < nums2.length; i++) {
-            if (map.containsKey(nums2[i]) && map.get(nums2[i]) > 0) {
-                list.add(nums2[i]);
-                map.put(nums2[i], map.get(nums2[i]) - 1);
+        int result[] = new int[nums2.length];
+        int k = 0;
+        for (int j = 0; j < nums2.length; j++) {
+            if (freq[nums2[j]] > 0) {
+                result[k] = nums2[j];
+                k++;
+                freq[nums2[j]]--;
             }
         }
 
-        int[] result = new int[list.size()];
-        int index = 0;
-        for (int num : list) {
-            result[index++] = num;
-        }
-
-        return result;
-
+        return Arrays.copyOf(result, k);
     }
 }
