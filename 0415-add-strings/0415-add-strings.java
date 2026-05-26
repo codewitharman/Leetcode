dfe07178a4ad218c53ccf3d2512cb1e1660119e6
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-05-26 11:08:15 +0530

    Time: 4 ms (22.19%), Space: 45 MB (19.47%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -1,19 +1,35 @@
 class Solution {
     public String addStrings(String num1, String num2) {
+
+
+        StringBuilder result = new StringBuilder();
+
+        int carry = 0;
+
         int i = num1.length() - 1;
         int j = num2.length() - 1;
-        int carry = 0;
-        StringBuilder sb = new StringBuilder();
-        while (i >= 0 || j >= 0 || carry != 0) {
-            int digit1 = i >= 0 ? num1.charAt(i) - '0' : 0;
-            int digit2 = j >= 0 ? num2.charAt(j) - '0' : 0;
-            int sum = digit1 + digit2 + carry;
-            carry = sum / 10;
-            sb.append(sum % 10);
+
+        while (i >= 0 || j >= 0) {
+
+            int num1Digit = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int num2Digit = j >= 0 ? num2.charAt(j) - '0' : 0;
+
+            int currSum = carry + num1Digit + num2Digit;
+
+            int nextDigit = currSum >= 10 ? currSum % 10 : currSum;
+            carry = currSum / 10;
+
+            result.insert(0, String.valueOf(nextDigit));
+
             i--;
             j--;
         }
 
-        return sb.reverse().toString();
+        if (carry == 1) {
+            result.insert(0, "1");
+        }
+
+        return result.toString();
+        
     }
 }
