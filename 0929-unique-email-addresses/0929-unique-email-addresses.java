a9fdc967eef6412bdfaf3d9ad5e94ed1988ee18b
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-06-15 15:32:49 +0530

    Time: 12 ms (72.48%), Space: 46.5 MB (76.02%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.java
+++ 0929-unique-email-addresses/0929-unique-email-addresses.java
@@ -5,16 +5,18 @@ class Solution {
             String[] parts = email.split("@");
             String local = parts[0];
             String domain = parts[1];
-
             int plusSignIndex = local.indexOf('+');
             if (plusSignIndex != -1) {
                 local = local.substring(0, plusSignIndex); //first part aaya
             }
             local = local.replace(".", "");
             String formattedEmail = local + "@" + domain;
-            map.put(formattedEmail, map.getOrDefault(formattedEmail, 0) + 1);
+            if (map.containsKey(formattedEmail)) {
+                map.put(formattedEmail, map.get(formattedEmail) + 1);
+            } else {
+                map.put(formattedEmail, 1);
+            }
         }
-
         return map.size();
     }
 }
