ddb93e4aa8873efd16e714ebf7d9ab727b48b81e
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-06-15 15:48:22 +0530

    Time: 5 ms (99.76%), Space: 46.4 MB (76.02%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.java
+++ 0929-unique-email-addresses/0929-unique-email-addresses.java
@@ -1,22 +1,25 @@
 class Solution {
     public int numUniqueEmails(String[] emails) {
-        Map<String, Integer> map = new HashMap<>();
+        Set<String> set = new HashSet<>();
         for (String email : emails) {
-            String[] parts = email.split("@");
-            String local = parts[0];
-            String domain = parts[1];
-            int plusSignIndex = local.indexOf('+');
-            if (plusSignIndex != -1) {
-                local = local.substring(0, plusSignIndex); //first part aaya
+            set.add(processedEmail(email));
+        }
+        return set.size();
+    }
+
+    public String processedEmail(String email) {
+        int domainIndex = email.indexOf("@");
+        StringBuilder sb = new StringBuilder(email.length());
+        for (int i = 0; i < domainIndex; i++) {
+            char ch = email.charAt(i);
+            if (ch == '+') {
+                break;
             }
-            local = local.replace(".", "");
-            String formattedEmail = local + "@" + domain;
-            if (map.containsKey(formattedEmail)) {
-                map.put(formattedEmail, map.get(formattedEmail) + 1);
-            } else {
-                map.put(formattedEmail, 1);
+            if (ch != '.') {
+                sb.append(ch);
             }
         }
-        return map.size();
+        sb.append(email.substring(domainIndex));
+        return sb.toString();
     }
 }
