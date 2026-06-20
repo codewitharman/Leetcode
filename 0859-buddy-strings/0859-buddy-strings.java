6e15e2de39e3725469e11c4d9c73cb6d5df60552
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-06-20 21:03:12 +0530

    Time: 1 ms (99.5%), Space: 43.6 MB (81.64%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean buddyStrings(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        if (s.equals(goal)) {
+            int[] count = new int[26];
+            for (int i = 0; i < s.length(); i++) {
+                count[s.charAt(i) - 'a']++;
+                if (count[s.charAt(i) - 'a'] > 1) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        int firstDiff = -1;
+        int secondDiff = -1;
+        int diffCount = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != goal.charAt(i)) {
+                diffCount++;
+                if (diffCount > 2) {
+                    return false; 
+                }
+                if (firstDiff == -1) {
+                    firstDiff = i;
+                } else {
+                    secondDiff = i;
+                }
+            }
+        }
+
+        return diffCount == 2 &&
+               s.charAt(firstDiff) == goal.charAt(secondDiff) &&
+               s.charAt(secondDiff) == goal.charAt(firstDiff);
+    }
+}
