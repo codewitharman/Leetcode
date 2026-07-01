3f8b8e9e7851a415e16e05204395b4e3df207a2d
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-07-01 16:50:24 +0530

    Time: 1 ms (100%), Space: 46.4 MB (25.78%) - LeetHub

--- 1629-slowest-key/1629-slowest-key.java
+++ 1629-slowest-key/1629-slowest-key.java
@@ -1,21 +1,18 @@
 class Solution {
     public char slowestKey(int[] releaseTimes, String keysPressed) {
-        Map<Character, Integer> map = new LinkedHashMap<>();
-        for (int i = 0; i < keysPressed.length(); i++) {
-            char ch = keysPressed.charAt(i);
-            int duration = (i == 0) ? releaseTimes[0]
-                    : releaseTimes[i] - releaseTimes[i - 1];
-            map.put(ch, Math.max(map.getOrDefault(ch, 0), duration));
-        }
+        int max = releaseTimes[0];
+        char ans = keysPressed.charAt(0);
 
-        int max = 0;
-        char key = '\u0000';
-        for (Map.Entry<Character, Integer> e : map.entrySet()) {
-            if (e.getValue() > max || (e.getValue() == max && e.getKey() > key)) {
-                max = e.getValue();
-                key = e.getKey();
+        for (int i = 1; i < releaseTimes.length; i++) {
+            int time = releaseTimes[i] - releaseTimes[i - 1];
+            char ch = keysPressed.charAt(i);
+
+            if (time > max || (time == max && ch > ans)) {
+                max = time;
+                ans = ch;
             }
         }
-        return key;
+
+        return ans;
     }
 }
