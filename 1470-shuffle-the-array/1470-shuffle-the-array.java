b59e8701eed5d469fb7e1185fa5839310155b099
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-07-01 12:16:46 +0530

    Time: 0 ms (100%), Space: 46.6 MB (41.41%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,26 +1,14 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int[] xArr = new int[nums.length / 2];
-        int[] yArr = new int[nums.length / 2];
+        int result[] = new int[n * 2];
         int index = 0;
-        for (int i = 0; i < n; i++) {
-            xArr[index++] = nums[i];
-        }
-        index = 0;
-        for (int i = n; i < nums.length; i++) {
-            yArr[index++] = nums[i];
-        }
-
-        int result[] = new int[nums.length];
-
-        for (int i = 0; i < xArr.length; i++) {
-            result[i * 2] = xArr[i];
-        }
-
-        for (int i = 0; i < xArr.length; i++) {
-            result[i * 2 + 1] = yArr[i];
+        for (int i = 0; i < nums.length / 2; i++) {
+            result[index] = nums[i];
+            result[index + 1] = nums[i + n];
+            index = index + 2;
         }
 
         return result;
+
     }
 }
