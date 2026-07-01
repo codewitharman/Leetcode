a8ea15dad6f1a0e738507653fb60ada404f86722
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-07-01 17:44:37 +0530

    Time: 5 ms (13.99%), Space: 46.8 MB (9.65%) - LeetHub

--- 3823-reverse-letters-then-special-characters-in-a-string/3823-reverse-letters-then-special-characters-in-a-string.java
+++ 3823-reverse-letters-then-special-characters-in-a-string/3823-reverse-letters-then-special-characters-in-a-string.java
@@ -4,21 +4,13 @@ class Solution {
         Stack<Character> st2 = new Stack<>();
 
         for (char ch : s.toCharArray()) {
-            if (ch >= 'a' && ch <= 'z') {
-                st1.push(ch);
-            } else {
-                st2.push(ch);
-            }
+            ((ch >= 'a' && ch <= 'z') ? st1 : st2).push(ch);
         }
 
         String ans = "";
         for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch >= 'a' && ch <= 'z') {
-                ans = ans + st1.pop();
-            } else {
-                ans = ans + st2.pop();
-            }
+            char ch=s.charAt(i);
+            ans = ans + ((ch >= 'a' && ch <= 'z') ? st1.pop() : st2.pop());
         }
 
         return ans;
