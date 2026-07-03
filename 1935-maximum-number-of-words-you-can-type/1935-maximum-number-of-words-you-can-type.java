c9b091e870680a208a73aa8cb3a1a613eb66e8c4
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-07-03 17:48:12 +0530

    Time: 2 ms (98.9%), Space: 43.6 MB (48.66%) - LeetHub

--- 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.java
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.java
@@ -1,25 +1,23 @@
 class Solution {
-    public int canBeTypedWords(String text, String brokenLetters) {
-        String[] arr = text.split(" ");
-        char[] chars = brokenLetters.toCharArray();
+    public int canBeTypedWords(String text, String broken) {
+        Set<Character> brokenKeys = new HashSet<>();
         int count = 0;
-        for (int i = 0; i < arr.length; i++) {
-            String word = arr[i];
-            boolean canType = true;          
-            int start = 0;
-            int end = chars.length;
-            while (start < end) {
-                char ch = chars[start];
-                if (word.indexOf(ch) >= 0) { 
-                    canType = false;
-                    break;                   
+
+        for (char c : broken.toCharArray()) {
+            brokenKeys.add(c);
+        }
+
+        String[] words = text.split(" ");
+
+        for (String word : words) {
+            for (char c : word.toCharArray()) {
+                if (brokenKeys.contains(c)) {
+                    count++;
+                    break;
                 }
-                start++;
-            }
-            if (canType) {
-                count++;                     
             }
         }
-        return count;
+
+        return words.length - count;
     }
 }
