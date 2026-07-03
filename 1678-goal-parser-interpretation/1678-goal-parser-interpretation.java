53c88bd8d328fc52ec0fecf927344cf55f0a6960
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-07-03 12:40:22 +0530

    Time: 1 ms (76.12%), Space: 42.9 MB (51.55%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,6 +1,5 @@
 class Solution {
     public String interpret(String command) {
-        command = command.replace("()", "o").replace("(al)", "al");
-        return command;
+        return command.replace("()", "o").replace("(al)", "al");
     }
 }
