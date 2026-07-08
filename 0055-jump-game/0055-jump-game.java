a20fd98c0976623bd8cae33bf0e01ca7f6b58a18
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-07-08 17:48:42 +0530

    Time: 1 ms (99.94%), Space: 48.1 MB (23.87%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,11 +1,12 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int goal = nums.length - 1;
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (i + nums[i] >= goal) {
-                goal = i;
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
             }
         }
-        return goal == 0;
+
+        return lastPos == 0;
     }
 }
