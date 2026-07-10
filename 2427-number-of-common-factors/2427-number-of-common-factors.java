32053143f3fbb7d794043e627482f4deb69720aa
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-07-10 17:54:00 +0530

    Time: 0 ms (100%), Space: 42.3 MB (6.43%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.java
+++ 2427-number-of-common-factors/2427-number-of-common-factors.java
@@ -1,22 +1,9 @@
 class Solution {
     public int commonFactors(int a, int b) {
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 1; i <= a; i++) {
-            if (a % i == 0) {
-                map.put(i, map.getOrDefault(i, 0) + 1);
-            }
-        }
-        for (int i = 1; i <= b; i++) {
-            if (b % i == 0) {
-                map.put(i, map.getOrDefault(i, 0) + 1);
-            }
-        }
-        int count = 0;
-        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
-            if (e.getValue() == 2) {
+        int min = Math.min(a, b), count = 0;
+        for (int i = 1; i <= min; i++) {
+            if (a % i == 0 && b % i == 0)
                 count++;
-            }
         }
 
         return count;
