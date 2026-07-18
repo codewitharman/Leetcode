9b955bcb55dee398f95c730c2280604452689af9
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-07-18 17:52:26 +0530

    Time: 1 ms (96.81%), Space: 44.1 MB (9.96%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = 0;
+        int[] temp = new int[candies.length];
+        List<Boolean> list = new ArrayList<>();
+        for (int i = 0; i < candies.length; i++) {
+            max = Math.max(candies[i], max);
+            temp[i] = candies[i] + extraCandies;
+        }
+
+        for (int candy : temp) {
+            if (candy >= max)
+                list.add(true);
+            else
+                list.add(false);
+        }
+
+        return list;
+    }
+}
