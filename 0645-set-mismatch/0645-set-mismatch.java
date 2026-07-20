1b00a03ce08b0dcf9ac95c30be4a98189061365e
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-07-20 22:00:05 +0530

    Time: 17 ms (16.27%), Space: 47.9 MB (17.48%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,21 +1,23 @@
+import java.util.*;
+
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int[] freq = new int[nums.length + 1];
-        int missing = -1;
-        int duplicate = -1;
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
-            freq[num]++;
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        for (int i = 1; i < freq.length; i++) {
-            if (freq[i] == 2) {
+        int duplicate = -1, missing = -1;
+        for (int i = 1; i <= n; i++) {
+            int count = map.getOrDefault(i, 0);
+            if (count == 2) {
                 duplicate = i;
-            } else if (freq[i] == 0) {
+            } else if (count == 0) {
                 missing = i;
             }
         }
 
         return new int[] { duplicate, missing };
-
     }
 }
