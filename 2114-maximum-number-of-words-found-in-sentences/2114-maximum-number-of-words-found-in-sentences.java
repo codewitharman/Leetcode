227f091002616ae5ca166ee5795449b762aae84b
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-07-23 10:01:18 +0530

    Time: 3 ms (87.31%), Space: 44.7 MB (58.02%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
@@ -1,15 +1,16 @@
 class Solution {
-    public int mostWordsFound(String[] sentences) {
+    public int mostWordsFound(String[] s) {
         int max = 0;
-        for (int i = 0; i < sentences.length; i++) {
-            String s = sentences[i];
-            String[] temp = s.split("\\s+");
-            int count = 0;
-
-            for (String str : temp) {
-                count++;
+        for (String str : s) {
+            int count = 1;
+            for (char ch : str.toCharArray()) {
+                if (ch == ' ') {
+                    count++;
+                }
             }
+
             max = Math.max(max, count);
+
         }
 
         return max;
