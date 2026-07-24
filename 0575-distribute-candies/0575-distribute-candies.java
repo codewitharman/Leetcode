5ec4636050c0f6de88f8b8c34702c103acfbf68e
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-07-24 22:21:24 +0530

    Time: 30 ms (92.21%), Space: 48 MB (98.61%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.java
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -1,20 +1,10 @@
 class Solution {
     public int distributeCandies(int[] candyType) {
-        int n = candyType.length;
-        int limit = n / 2;
-        int count = 0;
         Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < n; i++) {
-            set.add(candyType[i]);
+        for (int candy : candyType) {
+            set.add(candy);
         }
 
-        for (int candy : set) {
-            count++;
-            if (count == limit) {
-                return count;
-            }
-        }
-
-        return count;
+        return Math.min(set.size(), candyType.length / 2);
     }
 }
