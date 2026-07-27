83ba0ed19bb5bee980f44fb71baee29275149a3c
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-07-27 15:08:15 +0530

    Time: 1 ms (100%), Space: 43.7 MB (76.59%) - LeetHub

--- 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.java
+++ 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.java
@@ -1,23 +1,35 @@
 class Solution {
     public int maxFreqSum(String s) {
-        Map<Character, Integer> vowelMap = new HashMap<>();
-        Map<Character, Integer> consonantMap = new HashMap<>();
-        for (char ch : s.toCharArray()) 
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
-                vowelMap.put(ch, vowelMap.getOrDefault(ch, 0) + 1);
-            else
-                consonantMap.put(ch, consonantMap.getOrDefault(ch, 0) + 1);
-        
+        int[] freq = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            freq[ch - 'a']++;
+        }
+        int vowelMax = 0;
+        int consonantMax = 0;
+        for (int i = 0; i < freq.length; i++) {
+            if (i == 0 || i == 4 || i == 8 || i == 14 || i == 20) {
+                //char ch=(char) (i+(int)'a');
+                //System.out.println(ch+"-"+freq[i]);
+                if (freq[i] > vowelMax) {
+                    vowelMax = freq[i];
+                }
+            } else {
+                if (freq[i] > consonantMax) {
+                    consonantMax = freq[i];
+                }
+            }
+        }
+        return consonantMax + vowelMax;
+        //System.out.println(vowelMax);
 
-        int maxVowelValue = 0, maxConsonantValue = 0;
-        for (Map.Entry<Character, Integer> e : vowelMap.entrySet())
-            if (e.getValue() > maxVowelValue)
-                maxVowelValue = e.getValue();
+        // else {
+        //         if (freq[i] > consonantMax) {
+        //             consonantMax = freq[i];
+        //         }
+        //     }
+        //System.out.println(consonantMax);
+        //System.out.println(vowelMax);
 
-        for (Map.Entry<Character, Integer> e : consonantMap.entrySet())
-            if (e.getValue() > maxConsonantValue)
-                maxConsonantValue = e.getValue();
-
-        return maxVowelValue + maxConsonantValue;
     }
 }
