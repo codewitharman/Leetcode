0fbd74089d16ebdefadac59ba98187fc9b615c6e
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-07-27 15:11:05 +0530

    Time: 1 ms (100%), Space: 43.6 MB (85.64%) - LeetHub

--- 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.java
+++ 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.java
@@ -1,35 +1,19 @@
 class Solution {
     public int maxFreqSum(String s) {
         int[] freq = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
+        for (char ch : s.toCharArray())
             freq[ch - 'a']++;
-        }
-        int vowelMax = 0;
-        int consonantMax = 0;
+
+        int vowelMax = 0, consonantMax = 0;
         for (int i = 0; i < freq.length; i++) {
             if (i == 0 || i == 4 || i == 8 || i == 14 || i == 20) {
-                //char ch=(char) (i+(int)'a');
-                //System.out.println(ch+"-"+freq[i]);
-                if (freq[i] > vowelMax) {
-                    vowelMax = freq[i];
-                }
+                if (freq[i] > vowelMax) 
+                    vowelMax = freq[i];             
             } else {
-                if (freq[i] > consonantMax) {
-                    consonantMax = freq[i];
-                }
+                if (freq[i] > consonantMax) 
+                    consonantMax = freq[i];            
             }
         }
         return consonantMax + vowelMax;
-        //System.out.println(vowelMax);
-
-        // else {
-        //         if (freq[i] > consonantMax) {
-        //             consonantMax = freq[i];
-        //         }
-        //     }
-        //System.out.println(consonantMax);
-        //System.out.println(vowelMax);
-
     }
 }
