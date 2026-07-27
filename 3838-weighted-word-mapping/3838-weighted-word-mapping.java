95fa3df1a55fe623ce3986689736530014dd1c3e
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-07-27 16:09:40 +0530

    Time: 7 ms (9.49%), Space: 47.3 MB (6.81%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.java
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.java
@@ -2,36 +2,29 @@ class Solution {
     public String mapWordWeights(String[] words, int[] weights) {
 
         Map<Character, Integer> map = new HashMap<>();
-        char ch = 'a';
-        for (int i = 0; i < weights.length; i++) {
-            map.put(ch++, weights[i]);
-        }
-
         Map<Integer, Character> reverseAlpha = new HashMap<>();
+        char ch = 'a';
+        for (int i = 0; i < weights.length; i++) 
+            map.put(ch++, weights[i]);
+        
+
         char alpha = 'z';
-        for (int i = 0; i <= 25; i++) {
+        for (int i = 0; i <= 25; i++) 
             reverseAlpha.put(i, alpha--);
-        }
+        
         List<Integer> list = new ArrayList<>();
-        //System.out.println(reverseAlpha);
-        for (String word : words) {
-            int index = 0;
+         for (String word : words) {
             int sum = 0;
-            int resultModulo = 0;
-            while (index < word.length()) {
-                char c = word.charAt(index);
-                int value = map.get(c);
-                sum = sum + value;
-                index++;
+            for (int index = 0; index < word.length(); index++) {
+                sum = (sum + map.get(word.charAt(index))) % 26;
             }
-            resultModulo = sum % 26;
-            list.add(resultModulo);
+            list.add(sum);
         }
 
         StringBuilder result = new StringBuilder();
-        for (int value : list) {
+        for (int value : list) 
             result.append(reverseAlpha.get(value));
-        }
+        
 
         return result.toString();
     }
