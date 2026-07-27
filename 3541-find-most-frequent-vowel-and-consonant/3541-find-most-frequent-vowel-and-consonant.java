ee2fa59d4875ba4d150a63463d2937b7ee6d96f5
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-07-27 14:49:42 +0530

    Time: 4 ms (25.21%), Space: 44.3 MB (20.69%) - LeetHub

--- 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.java
+++ 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.java
@@ -2,13 +2,12 @@ class Solution {
     public int maxFreqSum(String s) {
         Map<Character, Integer> vowelMap = new HashMap<>();
         Map<Character, Integer> consonantMap = new HashMap<>();
-        for (char ch : s.toCharArray()) {
+        for (char ch : s.toCharArray()) 
             if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
                 vowelMap.put(ch, vowelMap.getOrDefault(ch, 0) + 1);
             else
                 consonantMap.put(ch, consonantMap.getOrDefault(ch, 0) + 1);
-
-        }
+        
 
         int maxVowelValue = 0, maxConsonantValue = 0;
         for (Map.Entry<Character, Integer> e : vowelMap.entrySet())
