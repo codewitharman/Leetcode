0bfd30963c48ec54a4b85819b470c8b0d22d75cf
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-07-30 15:26:44 +0530

    Time: 1 ms (97.5%), Space: 46.5 MB (26.93%) - LeetHub

--- 3701-compute-alternating-sum/3701-compute-alternating-sum.java
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.java
@@ -3,10 +3,9 @@ class Solution {
         int sum = 0;
         for (int i = 0; i < nums.length; i++) {
             if (i % 2 == 0)
-                sum = sum + nums[i];
+                sum +=nums[i];
             else
-                sum = sum - nums[i];
-
+                sum -= nums[i];
         }
 
         return sum;
