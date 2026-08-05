252d6b96e024f364acde4dca8bbadc6b58392a3d
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-08-05 11:06:20 +0530

    Time: 11 ms (5.75%), Space: 47.9 MB (6.25%) - LeetHub

--- 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
@@ -8,7 +8,6 @@ class Solution {
         for (int num : nums2) {
             map2.put(num, map2.getOrDefault(num, 0) + 1);
         }
-        //int[] result = new int[2];
         HashSet<Integer> set = new HashSet<>();
         HashSet<Integer> common = new HashSet<>();
         for (int num : nums1) {
@@ -16,19 +15,17 @@ class Solution {
         }
 
         for (int num : nums2) {
-            if (set.contains(num)) {
+            if (set.contains(num)) 
                 common.add(num);
-            }
+            
         }
 
-        //System.out.println(common);
         int sum1 = 0, sum2 = 0;
         for (int num : common) {
             sum1 = sum1 + map1.get(num);
-        }
-        for (int num : common) {
             sum2 = sum2 + map2.get(num);
         }
+        
         return new int[] { sum1, sum2 };
 
     }
