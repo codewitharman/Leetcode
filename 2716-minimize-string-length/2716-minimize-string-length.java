e0c86401023cf1268b12c790c52fa1b6e99b428d
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-08-12 10:22:26 +0530

    Time: 3 ms (99.91%), Space: 46.6 MB (78.34%) - LeetHub

--- 2716-minimize-string-length/2716-minimize-string-length.java
+++ 2716-minimize-string-length/2716-minimize-string-length.java
@@ -2,15 +2,12 @@ class Solution {
     public int minimizedStringLength(String s) {
         int[] freq = new int[26];
         int count = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
+        for (char ch : s.toCharArray())
             freq[ch - 'a']++;
-        }
-        for (int i = 0; i < freq.length; i++) {
-            if (freq[i] > 0) {
+
+        for (int i = 0; i < freq.length; i++)
+            if (freq[i] > 0)
                 count++;
-            }
-        }
 
         return count;
     }
