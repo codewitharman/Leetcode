0330343ce0160a053cc0dd9f4e3870a12cc1019a
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-08-13 17:07:20 +0530

    Time: 4 ms (1.43%), Space: 43.3 MB (20.84%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,16 +1,5 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        Set<Character> set = new HashSet<>();
-        for (char ch : jewels.toCharArray()) {
-            set.add(ch);
-        }
-        int count = 0;
-        for (int i = 0; i < stones.length(); i++) {
-            if (set.contains(stones.charAt(i))) {
-                count++;
-            }
-        }
-        return count;
-
+        return (int) stones.chars().filter(c -> jewels.indexOf(c) >= 0).count();
     }
 }
