da02ecd80d1442bba76b9888d0128f666276286b
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-08-13 16:53:08 +0530

    Time: 7 ms (5.26%), Space: 42.3 MB (99.17%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,17 +1,5 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        char[] freq = new char[26];
-        for (int i = 0; i < sentence.length(); i++) {
-            char ch = sentence.charAt(i);
-            freq[ch - 'a']++;
-        }
-
-        for (char ch : freq) {
-            if (ch == 0) {
-                return false;
-            }
-        }
-
-        return true;
+        return sentence.chars().distinct().count() == 26;
     }
 }
