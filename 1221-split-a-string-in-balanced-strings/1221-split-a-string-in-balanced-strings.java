60c7e4e3fe0ef9b415421c9e989d64e1ac048035
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-08-14 16:27:47 +0530

    Time: 2 ms (1.93%), Space: 42.7 MB (62.78%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        Stack<Character> st=new Stack<>();
+        int count=0;
+        for(char ch:s.toCharArray()){
+            if(st.isEmpty() || st.peek()==ch){
+                st.push(ch);
+            }else{
+                st.pop();
+            }
+            if(st.isEmpty()){
+                count++;
+            } 
+        }
+        return count;
+
+    }
+}
