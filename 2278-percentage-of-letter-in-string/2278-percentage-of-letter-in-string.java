8f7a7b34b7675806aa2c38fa930d2ad8fa11a672
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-08-16 23:26:51 +0530

    Time: 13 ms (0.64%), Space: 49.6 MB (7.9%) - LeetHub

--- 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.java
+++ 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.java
@@ -1,19 +1,16 @@
+import java.util.*;
+
 class Solution {
     public int percentageLetter(String s, char letter) {
         if (s.indexOf(letter) == -1) {
             return 0;
         }
-        Map<Character, Integer> map = new HashMap<>();
-        int occurrence = 0;
-        for (char ch : s.toCharArray()) {
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            if (entry.getKey() == letter) {
-                occurrence = entry.getValue();
-            }
-        }
-        return (occurrence * 100) / s.length();
+        Map<Character, Long> map = s.chars().mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        long occurrence = map.getOrDefault(letter, 0l);
+
+        return (int) (occurrence * 100) / s.length();
 
     }
 }
