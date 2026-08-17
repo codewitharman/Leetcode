684c7c03199a48a803ea706b4f00c417b72f7ea8
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-08-17 16:38:44 +0530

    Time: 5 ms (2.22%), Space: 42.3 MB (38.4%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.java
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.java
@@ -1,36 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int minimumSum(int num) {
-
-        int nums[] = new int[4];
-        int index = 0;
-        while (num > 0) {
-            int digit = num % 10;
-            nums[index++] = digit;
-            num = num / 10;
-        }
-        Optional<Integer> smallest = Arrays.stream(nums).boxed()
-                .sorted()
-                .findFirst();
-
-        Optional<Integer> secondSmallest = Arrays.stream(nums).boxed()
-                .sorted()
-                .skip(1)
-                .findFirst();
-
-        Optional<Integer> largest = Arrays.stream(nums)
-                .boxed()
-                .sorted(Comparator.reverseOrder())
-                .findFirst();
-
-        Optional<Integer> secondLargest = Arrays.stream(nums)
-                .boxed()
-                .sorted(Comparator.reverseOrder())
-                .skip(1)
-                .findFirst();
-        int result = ((smallest.orElse(0) * 10) + largest.orElse(0))
-                + ((secondSmallest.orElse(0) * 10) + secondLargest.orElse(0));
-        return result;
+        int[] nums = String.valueOf(num).chars().map(c -> c - '0').sorted().toArray();
+        return ((nums[0] * 10) + nums[3]) + ((nums[1] * 10) + nums[2]);
     }
 }
