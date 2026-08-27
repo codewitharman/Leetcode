55907012e092547f91cf287735a90adcb15ff87a
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-08-27 18:19:25 +0530

    Time: 1 ms (95.07%), Space: 42.7 MB (8.3%) - LeetHub

--- 3959-check-good-integer/3959-check-good-integer.java
+++ 3959-check-good-integer/3959-check-good-integer.java
@@ -5,21 +5,13 @@ class Solution {
         while (temp > 0) {
             int digit = temp % 10;
             digitSum += digit;
-            temp = temp / 10;
-        }
-        temp = n;
-        while (temp > 0) {
-            int digit = temp % 10;
-            squareSum = squareSum + (digit * digit);
+            squareSum += (digit * digit);
             temp = temp / 10;
         }
 
         int sum = squareSum - digitSum;
-        if (sum >= 50) {
-            return true;
-        } else {
-            return false;
-        }
+
+        return sum >= 50;
 
     }
 }
