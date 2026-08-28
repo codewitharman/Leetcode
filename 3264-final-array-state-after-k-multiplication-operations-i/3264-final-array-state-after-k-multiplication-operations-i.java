d3b8a549a347a327a1c1583fd8022c860fde6ef1
Arman Shaikh <107916172+codewitharman@users.noreply.github.com>
2026-08-28 14:43:05 +0530

    Time: 1 ms (98.89%), Space: 46.3 MB (97.06%) - LeetHub

--- 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.java
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.java
@@ -1,18 +1,17 @@
 class Solution {
     public int[] getFinalState(int[] nums, int k, int multiplier) {
-        int n = nums.length;
-        int index = 0;
-        while (index < k) {
-            int min = Integer.MAX_VALUE;
-            int smallestIndex = -1;
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] < min) {
-                    min = nums[i];
-                    smallestIndex = i;
+
+        while (k > 0) {
+
+            int minIndex = 0;
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] < nums[minIndex]) {
+                    minIndex = i;
                 }
             }
-            nums[smallestIndex] = nums[smallestIndex] * multiplier;
-            index++;
+            nums[minIndex] = nums[minIndex] * multiplier;
+
+            k--;
         }
 
         return nums;
